6f86026bc0be11ff8ce46f1a884b9021550dfb86
Vicente Agüero <vicenteaguero@uc.cl>
2025-02-13 23:49:06 -0300

    build(viafidei): :sparkles: Added Footer Component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+// website/src/components/Footer.jsx
+
+const Footer = () => {
+  return (
+    <footer className="footer no-select">
+      <p>
+        Via Fidei es un proyecto Open Source Católico creado por &nbsp;
+        <a href="https://vicenteaguero.com">Vicente Agüero</a>.
+      </p>
+      <p>
+        ¿Quieres colaborar en este proyecto? Visita la sección de &nbsp;
+        <a href="/elcaminodelafe">¿Qué es Via Fidei?</a>
+        &nbsp; para más información.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
